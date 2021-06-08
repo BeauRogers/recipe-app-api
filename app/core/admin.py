0988a66e9c93aa7b01a80cf9e8c71a3391f91122
BeauRogers <b.ac.rogers92@gmail.com>
2021-06-07 23:29:47 -0400

    Updated admin to support custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,35 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+"""
+The '_' is the recommended convention for converting strings into
+human readable text (this will help it get passed through the translation
+engine)
+"""
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        # each set of brackets is a section
+        ( ('Person Info'), {'fields':('name',)}), # need the ',' after 'name'
+        # otherwise it will think it's just a string
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        ( ('Important dates'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
